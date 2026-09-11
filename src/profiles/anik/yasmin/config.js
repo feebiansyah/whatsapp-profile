d285dbbede60c768aa55cf825f994ec5f3490760
feebiansyah <anandafeebiansyah2@gmail.com>
2026-09-11 17:27:54 +0700

    Add NEW LINK YASMIN

---
+++ src/profiles/anik/yasmin/config.js
@@ -0,0 +1,12 @@
+﻿import profileImage from './profile.jpg'
+
+// Contoh sementara: ganti ketiga tautan dengan tujuan asli karakter ini.
+export default {
+  name: 'Citra',
+  image: profileImage,
+  description: 'Lanjutkan untuk mengirim pesan atau melakukan Video Call langsung melalui WhatsApp.',
+  messageLink: 'https://s.shopee.co.id/20vN3TLDFe',
+  videoCallLink: 'https://s.shopee.co.id/20vN3TLDFe',
+  callLink: 'https://s.shopee.co.id/20vN3TLDFe',
+  downloadLink: 'https://s.shopee.co.id/20vN3TLDFe',
+}
