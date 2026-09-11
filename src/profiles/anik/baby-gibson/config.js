f74cf21af6792a8e6cfb1ea45e7a60c9b9a0b705
feebiansyah <anandafeebiansyah2@gmail.com>
2026-09-11 17:52:20 +0700

    Add NEW LINK BABY GIBSON, CECE

---
+++ src/profiles/anik/baby-gibson/config.js
@@ -0,0 +1,12 @@
+﻿import profileImage from './profile.jpg'
+
+// Contoh sementara: ganti ketiga tautan dengan tujuan asli karakter ini.
+export default {
+  name: 'Baby Gibson',
+  image: profileImage,
+  description: 'Lanjutkan untuk mengirim pesan atau melakukan Video Call langsung melalui WhatsApp.',
+  messageLink: 'https://s.shopee.co.id/1LfgHgJLzI',
+  videoCallLink: 'https://s.shopee.co.id/1LfgHgJLzI',
+  callLink: 'https://s.shopee.co.id/1LfgHgJLzI',
+  downloadLink: 'https://s.shopee.co.id/1LfgHgJLzI',
+}
---
+++ src/profiles/anik/cece/config.js
@@ -0,0 +1,12 @@
+﻿import profileImage from './profile.jpg'
+
+// Contoh sementara: ganti ketiga tautan dengan tujuan asli karakter ini.
+export default {
+  name: 'Cece',
+  image: profileImage,
+  description: 'Lanjutkan untuk mengirim pesan atau melakukan Video Call langsung melalui WhatsApp.',
+  messageLink: 'https://s.shopee.co.id/1Vz6UCwUp2',
+  videoCallLink: 'https://s.shopee.co.id/1Vz6UCwUp2',
+  callLink: 'https://s.shopee.co.id/1Vz6UCwUp2',
+  downloadLink: 'https://s.shopee.co.id/1Vz6UCwUp2',
+}
